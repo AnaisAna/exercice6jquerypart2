8cc8546bb58007fa22c762f8fdf9860129da1fed
AnaisAna <anais.fradin@laposte.net>
2019-02-04 14:32:20 +0100

    exercice6.html

---
+++ asset/js/script.js
@@ -0,0 +1,12 @@
+$('#green').mouseenter(function(){
+$('#text').css('color','green');
+});
+$('#red').mouseenter(function(){
+$('#text').css('color','red');
+});
+$('#blue').mouseenter(function(){
+$('#text').css('color','blue');
+});
+$('#blue, #red, #green').mouseleave(function(){
+  $('#text').css('color','black');
+})
